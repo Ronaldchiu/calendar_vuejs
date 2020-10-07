bf54a308c5ff05230aee1679250bb91180d04c72
ronaldchiu <ylzhao6930@163.com>
2020-10-07 21:56:08 +0800

    移动端初始配置

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
   <div id="app">
-    <div class="test">hello</div>
+      <router-view></router-view>
   </div>
 </template>
 
 <style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 .test {
   width: 100px;
   height: 100px;
