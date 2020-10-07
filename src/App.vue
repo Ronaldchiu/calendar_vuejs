7c78f691a29aa0e94b8439fdb22e5ad80d63e11f
ronaldchiu <ylzhao6930@163.com>
2020-10-07 21:35:20 +0800

    移动端初始配置

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,11 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 .test {
   width: 100px;
   height: 100px;
+  font-size: 24px;
+  color: #ffff;
   background: blue;
 }
 </style>
